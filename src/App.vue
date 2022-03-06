46474c9fac21a554e2893ac1da54fce857e305df
Osama-kn <oussamaknouz2000@gmail.com>
2022-03-06 16:55:20 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,139 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      :showAddTask="showAddTask"
+      title="Task Tracker"
+    />
+    <div v-show="showAddTask">
+      <AddTask @toggle-add-task="toggleAddTask" @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+import AddTask from "./components/AddTask.vue";
+
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [] as any,
+      showAddTask: false,
+    };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+    async addTask(task: Object) {
+      const res = await fetch('api/tasks', { 
+        method: 'POST',
+        headers: { 
+          'Content-Type': 'application/json',
+        },
+          body : JSON.stringify(task)
+      })
+
+      const data = await res.json()
+      this.tasks = [...this.tasks, data];
+    },
+    async deleteTask(id: any) {
+      if (confirm("Are you sure you want to delete this")){
+        const res = await fetch(`api/tasks/${id}`,{
+          method : "DELETE",
+
+        })
+        res.status === 200 ? (this.tasks = this.tasks.filter((task: any) => task.id !== id)) : alert('Error deleting task') 
+      }
+    },
+    async toggleReminder(id: any) {
+      const taskToggle = await this.fetchTask(id)
+      const updTask = {...taskToggle, reminder: !taskToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`,{
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updTask)
+      })
+      const data = await res.json()
+      this.tasks = this.tasks.map((task: any) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      );
+    },
+    async fetchTasks(){
+      const res = await fetch('api/tasks')
+      const data = await res.json()
+      return data
+    },
+     async fetchTask(id:any){
+      const res = await fetch(`api/tasks/${id}`)
+      const data = await res.json()
+      return data
+    },
+  },
+  async created() {
+    this.tasks = await this.fetchTasks()
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
